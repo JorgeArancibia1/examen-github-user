2630a58691e6b90a830b5153ebbc89000577f088
koke <jorge.arancibia.jeal@gmail.com>
2024-04-26 02:49:50 -0400

    [feature]: Add useForm to GitHubUser component

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -1,16 +1,38 @@
-
+import { useForm } from "@tanstack/react-form";
 
 export const GitHubUser = () => {
+	const form = useForm({
+		defaultValues: {
+			searchName: "",
+		},
+		onSubmit: async ({ value }) => {
+			console.log(value.searchName);
+		},
+	});
+	console.log(form.state.values.searchName);
 
-  return (
-    <form>
-      <input
-        type="text"
-        placeholder="Busca un usuario de Gsithub"
-      />
-      <button>
-        Buscar
-      </button>
-    </form>
-  )
-}
+	return (
+		<form
+			onSubmit={(e) => {
+				e.preventDefault();
+				e.stopPropagation();
+				form.handleSubmit();
+			}}
+		>
+			<div>
+				<form.Field
+					name='searchName'
+					children={(field) => (
+						<input
+							name={field.name}
+							value={field.state.value}
+							onBlur={field.handleBlur}
+							onChange={(e) => field.handleChange(e.target.value)}
+						/>
+					)}
+				/>
+			</div>
+			<button type='submit'>Buscar</button>
+		</form>
+	);
+};
