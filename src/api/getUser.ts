bc990836666c5a6413b467472ccd64c5f97d2395
koke <jorge.arancibia.jeal@gmail.com>
2024-04-28 00:18:42 -0400

    [refactor]: Refactorize baseURL from githubApi

--- src/api/getUser.ts
+++ src/api/getUser.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 export const githubApi = axios.create({
-  baseURL: "https://api.github.com/",
+  baseURL: "https://api.github.com",
   headers: {
     "Content-Type": "application/json",
+    // "User-Agent": "examen-github-user",
     // Authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
     Authorization: `Bearer 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`,
   },
