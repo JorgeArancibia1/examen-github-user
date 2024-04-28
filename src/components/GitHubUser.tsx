8eeafa8f9585add45fc80d34c8dd2100cc3ec2c0
koke <jorge.arancibia.jeal@gmail.com>
2024-04-28 01:41:40 -0400

    [refactor]: Refactorize clean button in input GitHubUser

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -1,9 +1,11 @@
 import { useForm } from "@tanstack/react-form";
 import confetti from "canvas-confetti";
+import clsx from "clsx";
 import { FC, useEffect } from "react";
 import { useUser } from "../hooks/useFetchUser";
 import { ReposUserCard } from "./ReposUserCard";
 import { UserCard } from "./UserCard";
+import { CloseIcon } from "./ui/CloseIcon";
 import { Loading } from "./ui/Loading";
 
 export const GitHubUser: FC = () => {
@@ -13,7 +15,9 @@ export const GitHubUser: FC = () => {
 		},
 	});
 
-	const { user, isLoading, repos, isLoadingRepos } = useUser(form.state.values.searchName);
+	const { user, isLoading, repos, isLoadingRepos } = useUser(
+		form.state.values.searchName,
+	);
 
 	const handleClick = () => {
 		form.reset();
@@ -35,7 +39,7 @@ export const GitHubUser: FC = () => {
 				e.preventDefault();
 				e.stopPropagation();
 				form.handleSubmit();
-				if ( user !== null) {
+				if (user !== null) {
 					confetti({
 						particleCount: 100,
 						spread: 70,
@@ -48,13 +52,26 @@ export const GitHubUser: FC = () => {
 				<form.Field
 					name='searchName'
 					children={(field) => (
-						<input
-							className='border bg-slate-500 rounded-lg p-2 w-1/2'
-							name={field.name}
-							value={field.state.value}
-							onBlur={field.handleBlur}
-							onChange={({ target: { value } }) => field.handleChange(value)}
-						/>
+						<label
+							htmlFor='email'
+							className='relative text-gray-400 focus-within:text-gray-600 block'
+						>
+							<CloseIcon
+								onClick={handleClick}
+								className={clsx({
+									hidden: form.state.values.searchName === "",
+									"w-8 h-7 absolute top-1/2 transform -translate-y-1/2 right-2 cursor-pointer":
+										form.state.values.searchName !== "",
+								})}
+							/>
+							<input
+								className='border bg-slate-500 rounded-lg p-2  '
+								name={field.name}
+								value={field.state.value}
+								onBlur={field.handleBlur}
+								onChange={({ target: { value } }) => field.handleChange(value)}
+							/>
+						</label>
 					)}
 				/>
 				<button
@@ -62,13 +79,6 @@ export const GitHubUser: FC = () => {
 					type='submit'
 				>
 					Buscar
-				</button>
-				<button
-					className='rounded-lg font-medium bg-red-400 cursor-pointer border-hidden px-5'
-					type='button'
-					onClick={handleClick}
-				>
-					Limpiar
 				</button>
 			</div>
 			{isLoading && <Loading />}
