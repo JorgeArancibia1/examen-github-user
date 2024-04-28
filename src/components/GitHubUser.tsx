621a88e42f83505b7ec173c2110ebaa55d35a8d0
koke <jorge.arancibia.jeal@gmail.com>
2024-04-28 00:12:34 -0400

    [feature]: Add created components and validate in cases without information

--- src/components/GitHubUser.tsx
+++ src/components/GitHubUser.tsx
@@ -1,15 +1,19 @@
 import { useForm } from "@tanstack/react-form";
+import { FC } from "react";
+import { useUser } from "../hooks/useFetchUser";
+import { ReposUserCard } from "./ReposUserCard";
+import { UserCard } from "./UserCard";
 
-export const GitHubUser = () => {
+export const GitHubUser: FC = () => {
 	const form = useForm({
 		defaultValues: {
 			searchName: "",
 		},
-		onSubmit: async ({ value }) => {
-			console.log(value.searchName);
-		},
 	});
-	console.log(form.state.values.searchName);
+
+	const { user, isLoading, repos } = useUser(
+		form.state.values.searchName,
+	);
 
 	return (
 		<form
@@ -19,20 +23,47 @@ export const GitHubUser = () => {
 				form.handleSubmit();
 			}}
 		>
-			<div>
+			<div className='flex justify-center my-8 gap-4'>
 				<form.Field
 					name='searchName'
 					children={(field) => (
 						<input
+							className='border bg-slate-500 rounded-lg p-2 w-1/2'
 							name={field.name}
 							value={field.state.value}
 							onBlur={field.handleBlur}
-							onChange={(e) => field.handleChange(e.target.value)}
+							onChange={({target: { value }}) => field.handleChange(value)}
 						/>
 					)}
 				/>
+				<button
+					className='rounded-lg font-medium bg-indigo-600 cursor-pointer border-hidden px-5'
+					type='submit'
+				>
+					Buscar
+				</button>
 			</div>
-			<button type='submit'>Buscar</button>
+			{isLoading && <p>Loading...</p>}
+			{!isLoading && user === null && form.state.values.searchName !== "" && (
+				<p>No se ha encontrado un usuario con ese nombre</p>
+			)}
+
+			{/* Si viene informacion */}
+			{!!user && !!repos && (
+				<div className='flex gap-2 flex-wrap justify-center'>
+					<UserCard info={user} />
+					<div>
+						<p className='text-3xl font-bold my-4'>Proyectos</p>
+						{repos.length === 0 ? (
+							<p>
+								No se han encontrado repositorios existentes para este usuario
+							</p>
+						) : (
+							<ReposUserCard repos={repos} />
+						)}
+					</div>
+				</div>
+			)}
 		</form>
 	);
 };
