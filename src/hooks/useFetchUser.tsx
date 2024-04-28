50da63204e9e4f49ce5e9177f27d473f4d7a8338
koke <jorge.arancibia.jeal@gmail.com>
2024-04-28 00:08:03 -0400

    [feature]: Add reposQuery to get repositories

--- src/hooks/useFetchUser.tsx
+++ src/hooks/useFetchUser.tsx
@@ -1,25 +1,50 @@
 // import { useQuery } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { githubApi } from "../api/getUser";
-import { User } from "../interfaces/user";
+import { Repo, User } from "../interfaces";
 
-const getUserInfo = async(searchName: string = "JorgeArancibia1"): Promise<User> => {
+const getUserInfo = async (searchName: string): Promise<User | void > => {
+	try {
+		const { data } = await githubApi.get<User>(
+			`users/${searchName}`,
+		);
 
-  const { data } =  await githubApi.get<User>(`users/${searchName}`)
-  console.log(data)
+		return data;
+	} catch (error) {
+		const err = error as AxiosError
+		console.log(err.response?.status, err.message);
+		if (err.response?.status === 500) {
+			throw new Error("Error en el servidor, por favor contactar con soporte");
+		}
+	}
+};
 
-  return data;
-}
+const getReposUser = async (searchName: string): Promise<Repo[] | void> => {
+	const { data } = await githubApi.get(
+		`users/${searchName}/repos?sort=created&per_page=4`,
+	);
+	return data;
+};
 
 export const useUser = (searchName: string) => {
+	const userQuery = useQuery({
+		queryKey: ["user", "searchName", searchName],
+		queryFn: () => getUserInfo(searchName),
+		enabled: searchName !== "",
+	});
 
-  const userQuery = useQuery({
-    queryKey: ['user', searchName],
-    queryFn: () => getUserInfo(searchName)
-  })
+	const reposQuery = useQuery({
+		queryKey: ["repos", "searchName", searchName, "repos"],
+		queryFn: () => getReposUser(searchName),
+		enabled: !!userQuery.data,
+	});
 
-  return {
-    user: userQuery.data
-  }
-
-}
+	return {
+		isLoading: userQuery.isLoading,
+		isError: userQuery.isError,
+		error: userQuery.error?.message,
+		user: userQuery.data || null,
+		repos: reposQuery.data || [],
+	};
+};
